c90bbc1636d4f945d2eb795b1116041d9ccdecfb
水原樹 <>
2025-02-24 17:11:10 +0900

    first commit

---
+++ Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,17 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import SwiftUI
+
+@available(iOS 13.0,*)
+public struct MyLibrary {
+    public init(){
+        
+    }
+    
+    public var body: some View {
+        HStack {
+            Text("Hello, World!")
+        }
+    }
+}
